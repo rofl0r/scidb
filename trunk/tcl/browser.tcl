f5f68af25f904b77d865aa55e639901d8ec786d6
gcramer <gcramer@localhost>
2012-01-16 16:42:18 +0000

    r195|bug in browser reverted

--- trunk/tcl/browser.tcl
+++ trunk/tcl/browser.tcl
@@ -1,7 +1,7 @@
 # ======================================================================
 # Author : $Author$
-# Version: $Revision: 193 $
-# Date   : $Date: 2012-01-16 09:55:54 +0000 (Mon, 16 Jan 2012) $
+# Version: $Revision: 195 $
+# Date   : $Date: 2012-01-16 16:42:18 +0000 (Mon, 16 Jan 2012) $
 # Url    : $URL$
 # ======================================================================
 
@@ -500,8 +500,8 @@ proc NextGame {parent position {step 0}} {
 	}
 	ConfigureButtons $position
 	SetTitle $position
-#	set Vars(number) [::scidb::db::get gameNumber $Vars(base) $Vars(index) $Vars(view)]
-	::widget::busyOperation ::game::load $parent $position $Vars(base) $Vars(number)
+	set number [::scidb::db::get gameNumber $Vars(base) $Vars(index) $Vars(view)]
+	::widget::busyOperation ::game::load $parent $position $Vars(base) $number
 	::scidb::game::go $position position $Vars(fen)
 	UpdateHeader $position
 }
