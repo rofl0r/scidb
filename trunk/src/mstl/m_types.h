3b22c3542595dc63990de787addc6d5e050515c7
gcramer <gcramer@localhost>
2012-06-25 18:10:14 +0000

    r359|some changes for clang support

--- trunk/src/mstl/m_types.h
+++ trunk/src/mstl/m_types.h
@@ -1,7 +1,7 @@
 // ======================================================================
 // Author : $Author$
-// Version: $Revision: 346 $
-// Date   : $Date: 2012-06-15 21:04:20 +0000 (Fri, 15 Jun 2012) $
+// Version: $Revision: 359 $
+// Date   : $Date: 2012-06-25 18:10:14 +0000 (Mon, 25 Jun 2012) $
 // Url    : $URL$
 // ======================================================================
 
@@ -46,6 +46,10 @@
 # endif
 #endif
 
+#ifdef __clang__
+# define __CLANG_PREREQ(maj, min) ((__clang_major__ << 16) + __clang_minor__ >= ((maj) << 16) + (min))
+#endif
+
 #ifndef INT64_C
 # if __WORDSIZE == 64
 #  define INT64_C(c)		c ## L
@@ -57,10 +61,69 @@
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
-// C++Ox standard (reference: http://gcc.gnu.org/projects/cxx0x.html)
+// C++Ox standard (reference: http://gcc.gnu.org/projects/cxx0x.html
+//                            http://clang.llvm.org/cxx_status.html)
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
+#ifdef __clang__
+
+# if __CLANG_PREREQ(2,9) ///////////////////////////////////////////////////////
+# define HAVE_0X_RVALUE_REFERENCES 1
+# define HAVE_0X_INITIALIZATION_OF_CLASS_OBJECTS_BY_RVALUES 1
+# define HAVE_0X_VARIADIC_TEMPLATES 1
+# define HAVE_0X_EXTENDED_VARIADIC_TEMPLATE_PARAMETERS 1
+# define HAVE_0X_STATIC_ASSERTIONS 1
+# define HAVE_0X_AUTO_TYPED_VARIABLES 1
+# define HAVE_0X_MULTI_DECLARATOR_AUTO 1
+# define HAVE_0X_REMOVAL_OF_AUTO_AS_A_STORAGE_CLASS_SPECIFIER 1
+# define HAVE_0X_NEW_FUNCTION_DECLARATOR_SYNTAX 1
+# define HAVE_0X_DECLARED_TYPE_OF_AN_EXPRESSSION 1
+# define HAVE_0X_RIGHT_ANGLE_BRACKET 1
+# define HAVE_0X_DEFAULT_TEMPLATE_ARGUMENTS_FOR_FUNCTION_TEMPLATES 1
+# define HAVE_0X_SOLVING_THE_SFINAE_PROBLEM_FOR_EXPRESSIONS 1
+# define HAVE_0X_EXTERN_TEMPLATES 1
+# define HAVE_0X_STRONGLY_TYPED_ENUMS 1
+# define HAVE_0X_NEW_CHARACTER_TYPES 1
+# define HAVE_0X_EXTENDED_FRIEND_DECLARATIONS 1
+# define HAVE_0X_INLINE_NAMESPACES 1
+# define HAVE_0X_LOCAL_AND_UNNAMED_TYPES_AS_TEMPLATE_ARGUMENTS 1
+# define HAVE_0X_PROPAGATING_EXCEPTIONS 1
+# endif
+
+# if __CLANG_PREREQ(3,0) ///////////////////////////////////////////////////////
+# define HAVE_0X_NON_STATIC_DATA_MEMBER_INITIALIZERS 1
+# define HAVE_0X_TEMPLATE_ALIASES
+# define HAVE_0X_NULL_POINTER_CONSTANT 1
+# define HAVE_0X_ALIGNMENT_SUPPORT 1
+# define HAVE_0X_DELEGATING_CONSTRUCTORS 1
+# define HAVE_0X_EXPLICIT_CONVERSION_OPERATORS 1
+# define HAVE_0X_UNICODE_STRING_LITERALS 1
+# define HAVE_0X_RAW_STRING_LITERALS 1
+# define HAVE_0X_STANDARD_LAYOUT_TYPES 1
+# define HAVE_0X_DEFAULTED_AND_DELETED_FUNCTIONS 1
+# define HAVE_0X_RANGE_BASED_FOR 1
+# define HAVE_0X_EXPLICIT_VIRTUAL_OVERRRIDES 1
+# define HAVE_0X_ALLOWING_MOVE_CONSTRUCTORS_TO_THROW 1
+# define HAVE_0X_DEFINING_MOVE_SPECIAL_MEMBER_FUNCTIONS 1
+# endif
+
+# if __CLANG_PREREQ(3,1) ///////////////////////////////////////////////////////
+# define HAVE_0X_INITIALIZER_LISTS 1
+# define HAVE_0X_LAMBDA_EXPRESSIONS 1
+# define HAVE_0X_INCOMPLETE_RETURN_TYPES 1
+# define HAVE_0X_FORWARD_DECLARATIONS_FOR_ENUMS 1
+# define HAVE_0X_GENERALIZED_CONSTANT_EXPRESSIONS 1
+# define HAVE_0X_UNIVERSAL_CHARACTER_NAME_LITERALS 1
+# define HAVE_0X_USER_DEFINED_LITERALS 1
+# define HAVE_0X_EXTENDING_SIZEOF 1
+# define HAVE_0X_UNRESTRICTED_UNIONS 1
+# define HAVE_0X_ATOMIC_OPERATORS 1
+# define HAVE_0X_STRONG_COMPARE_AND_EXCHANGE 1
+# define HAVE_0X_BIDIRECTIONAL_FENCES 1
+# define HAVE_0X_ALLOW_ATOMICS_USE_IN_SIGNAL_HANDLERS 1
+# endif
+
+#elif defined(__GXX_EXPERIMENTAL_CXX0X__)
 
 #ifndef USE_0X_STANDARD
 // IMPORTANT NOTE: not really working with compiler versions < 4.6
@@ -117,7 +180,7 @@
 # define HAVE_0X_STRONGLY_TYPED_ENUMS 1
 # define HAVE_0X_NEW_CHARACTER_TYPES 1
 # define HAVE_0X_DEFAULTED_AND_DELETED_FUNCTIONS 1
-# define HAVE_0X_EXTENDED_SIZEOF 1
+# define HAVE_0X_EXTENDING_SIZEOF 1
 # define HAVE_0X_INLINE_NAMESPACES 1
 # define HAVE_0X_ATOMIC_OPERATORS 1
 # define HAVE_0X_PROPAGATING_EXCEPTIONS 1
@@ -215,8 +278,8 @@
 #ifndef HAVE_0X_DEFAULTED_AND_DELETED_FUNCTIONS
 # define HAVE_0X_DEFAULTED_AND_DELETED_FUNCTIONS 0
 #endif
-#ifndef HAVE_0X_EXTENDED_SIZEOF
-# define HAVE_0X_EXTENDED_SIZEOF 0
+#ifndef HAVE_0X_EXTENDING_SIZEOF
+# define HAVE_0X_EXTENDING_SIZEOF 0
 #endif
 #ifndef HAVE_0X_INLINE_NAMESPACES
 # define HAVE_0X_INLINE_NAMESPACES 0
@@ -286,6 +349,12 @@
 #endif
 #ifndef HAVE_USER_DEFINED_LITERALS
 # define HAVE_USER_DEFINED_LITERALS 0
+#endif
+#ifndef HAVE_0X_LAMBDA_EXPRESSIONS
+# define HAVE_0X_LAMBDA_EXPRESSIONS 0
+#endif
+#ifndef HAVE_0X_INCOMPLETE_RETURN_TYPES
+# define HAVE_0X_INCOMPLETE_RETURN_TYPES 0
 #endif
 
 // C++Ox definitions //////////////////////////////////////////////////////////
