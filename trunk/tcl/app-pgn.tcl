b2288f62d7bcdc872820fd664ca9fe3ea10d6390
gcramer <gcramer@localhost>
2012-06-27 09:02:22 +0000

    r361|bug in app-pgn.tcl fixed

--- trunk/tcl/app-pgn.tcl
+++ trunk/tcl/app-pgn.tcl
@@ -1,7 +1,7 @@
 # ======================================================================
 # Author : $Author$
-# Version: $Revision: 360 $
-# Date   : $Date: 2012-06-26 17:02:51 +0000 (Tue, 26 Jun 2012) $
+# Version: $Revision: 361 $
+# Date   : $Date: 2012-06-27 09:02:22 +0000 (Wed, 27 Jun 2012) $
 # Url    : $URL$
 # ======================================================================
 
@@ -144,12 +144,15 @@ array set Options {
 	diagram-show		1
 	diagram-pad-x		25
 	diagram-pad-y		5
+	tabstop-1			6.0
+	tabstop-2			0.7
+	tabstop-3			12.0
 }
 
 variable Vars
 variable CharLimit 250
 variable Counter 0
-variable MoveStyles { san lan alg cor tel }
+variable MoveStyles { san lan alg eng cor tel }
 
 
 proc build {parent width height} {
@@ -2687,7 +2690,7 @@ proc Refresh {var} {
 		}
 	}
 
-	::widget::busyOperation ::scidb::game::refresh
+	::widget::busyOperation ::scidb::game::refresh -all
 }
 
 
