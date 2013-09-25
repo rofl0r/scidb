7de33220604b0d2162819df91e1470ce9fb5712a
gcramer <gcramer@localhost>
2013-09-25 23:18:42 +0000

    r955|some fixes

--- trunk/scripts/make-package.sh
+++ trunk/scripts/make-package.sh
@@ -4,7 +4,7 @@ shift
 checkinstall --type=$type \
 	--pkgname="scidb-beta" \
 	--pkgversion="1.0" \
-	--pkgrelease="r953" \
+	--pkgrelease="r955" \
 	--pkglicense="GPL" \
 	--pkggroup="Application/Games" \
 	--pkgsource="http://sourceforge.net/projects/scidb" \
