1576a9f1af0b3a4e1885ea0f16849d7e4d3b4ade
gcramer <gcramer@localhost>
2013-09-30 15:32:14 +0000

    r958|fixes and tweaks; upgrade to r958

--- trunk/scripts/make-package.sh
+++ trunk/scripts/make-package.sh
@@ -3,12 +3,12 @@ type=$1
 shift
 requires=""
 if [ "$type" = "debian" ]; then
-	requires="tk8.5,tcl8.5,libX11-6,libsm6,libice6,libxcursor1,libexpat1,libstdc++6,libc6,libgcc1,libxft2,libfontconfig1,libfreetype6,zlib1g,desktop-file-utils,xdg-util"
+	requires="tk8.5,tcl8.5,libX11-6,libsm6,libice6,libxcursor1,libexpat1,libstdc++6,libc6,libgcc1,libxft2,libfontconfig1,libfreetype6,zlib1g,desktop-file-utils,xdg-utils"
 fi
 checkinstall --type=$type \
 	--pkgname="scidb-beta" \
 	--pkgversion="1.0" \
-	--pkgrelease="r957" \
+	--pkgrelease="r958" \
 	--pkglicense="GPL" \
 	--pkggroup="Application/Games" \
 	--pkgsource="http://sourceforge.net/projects/scidb" \
