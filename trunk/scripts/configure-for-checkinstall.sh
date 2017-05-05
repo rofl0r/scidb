5508982a6fe1b81b0d1e19b0756d6677987d1cfb
gcramer <gcramer@localhost>
2017-05-05 17:01:14 +0000

    r1147|Fix for packaging.

--- trunk/scripts/configure-for-checkinstall.sh
+++ trunk/scripts/configure-for-checkinstall.sh
@@ -1,1 +1,1 @@
-./configure --tcl-version=8.5 --force-bundled-zziplib=yes --force-bundled-gdbm=yes --destdir=/usr/local $*
+./configure --tcl-version=8.5 --force-bundled-zziplib=yes --force-bundled-gdbm=yes --destdir=/ $*
